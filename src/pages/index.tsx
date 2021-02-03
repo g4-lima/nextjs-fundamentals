0fd7c9bd937a7e672a016711266064a7302a5a15
Gabriel Lima <gabastos.lima@gmail.com>
2021-02-03 20:45:53 -0300

    serverSideRendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react'
+import { GetServerSideProps } from 'next'
 import { Title } from '../styles/Pages/Home'
 
 interface IProduct {
@@ -7,16 +7,12 @@ interface IProduct {
     title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-     fetch('http://localhost:3333/recommended').then(response => {
-       response.json().then(data => {
-         setRecommendedProducts(data)
-       })
-     })
-  }, [])
+export default function Home({ recommendedProducts }: HomeProps) {
+  
 
   return (
     <div>
@@ -35,4 +31,15 @@ export default function Home() {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended')
+  const recommendedProducts = await response.json()
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
